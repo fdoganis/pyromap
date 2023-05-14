362ac31165b15e75e46346f4dd278dc7da30e8ac
fdoganis <fivos.doganis@gmail.com>
2023-05-14 20:45:23 +0200

    fix animation loop

--- main.js
+++ main.js
@@ -171,8 +171,6 @@ function handleController(controller) {
 
 function animate() {
 
-  renderer.setAnimationLoop(render);
-
   const delta = clock.getDelta();
   if (delta >= 0.1) {
     if (cone != null) {
